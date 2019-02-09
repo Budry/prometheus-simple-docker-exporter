205e5b6e12a70118f416024ee22b58c392e1f9c6
Budry <zarubaondra@gmail.com>
2019-02-09 15:57:41 +0100

    Update dependency

---
+++ src/collectors/docker.go
@@ -0,0 +1,92 @@
+package collectors
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/Budry/prometheus-simple-docker-exporter/src/utils"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	namespace = "docker"
+	labels = []string{"container", "name", "project"}
+	memoryUsage = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace,"", "memory_usage_bytes"),
+		"Current memory usage in bytes",
+		labels, nil,
+	)
+	memoryLimit = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace,"", "memory_limit_bytes"),
+		"Current memory usage",
+		labels, nil,
+	)
+	cpuUsagePercent = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace,"", "cpu_usage_percent"),
+		"Current CPU usage in percent",
+		labels, nil,
+	)
+)
+
+
+type DockerStatsCollector struct{}
+
+func (exporter *DockerStatsCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- memoryUsage
+	ch <- memoryLimit
+	ch <- cpuUsagePercent
+}
+
+func (exporter *DockerStatsCollector) Collect(ch chan<- prometheus.Metric) {
+
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Println(container.Labels["com.docker.compose.project"])
+
+		stats, err := cli.ContainerStats(ctx, container.ID, false)
+		if err != nil {
+			panic(err)
+		}
+		//decoder := json.NewDecoder(stats.Body)
+		//containerStats := &types.Stats{}
+		//for {
+		//	select {
+		//	case <-ctx.Done():
+		//		stats.Body.Close()
+		//		fmt.Println("Stop logging")
+		//		return
+		//	default:
+		//		if err := decoder.Decode(containerStats); err == io.EOF {
+		//			return
+		//		} else if err != nil {
+		//			cancel()
+		//		}
+		//		fmt.Println(containerStats.CPUStats.CPUUsage.TotalUsage)
+		//	}
+		//}
+
+		s := &types.Stats{}
+		json.NewDecoder(stats.Body).Decode(s)
+
+		ch <- prometheus.MustNewConstMetric(memoryUsage, prometheus.GaugeValue, float64(s.MemoryStats.Usage), container.ID, container.Names[0], container.Labels["com.docker.compose.project"])
+		ch <- prometheus.MustNewConstMetric(memoryLimit, prometheus.GaugeValue, float64(s.MemoryStats.Limit), container.ID, container.Names[0], container.Labels["com.docker.compose.project"])
+		ch <- prometheus.MustNewConstMetric(cpuUsagePercent, prometheus.GaugeValue, utils.CalculateCPUPercentUnix(s.PreCPUStats, s.CPUStats), container.ID, container.Names[0], container.Labels["com.docker.compose.project"])
+	}
+}
+
+func NewDockerStatsCollector() *DockerStatsCollector {
+	return &DockerStatsCollector{}
+}
--- src/main.go
+++ src/main.go
@@ -1,77 +1,16 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"github.com/Budry/prometheus-simple-docker-exporter/src/utils"
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
+	"github.com/Budry/prometheus-simple-docker-exporter/src/collectors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 )
 
-var (
-	namespace   = "docker"
-	labels      = []string{"container", "name", "project"}
-	memoryUsage = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: namespace,
-		Name:      "memory_usage_bytes",
-		Help:      "Current memory usage in bytes.",
-	}, labels,
-	)
-	memoryLimit = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: namespace,
-		Name:      "memory_limit_bytes",
-		Help:      "Memory limit for container in bytes.",
-	}, labels,
-	)
-	cpuUsagePercent = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: namespace,
-		Name:      "cpu_usage_percent",
-		Help:      "Current CPU usage in percent.",
-	}, labels,
-	)
-)
-
-func init() {
-	prometheus.MustRegister(memoryUsage)
-	prometheus.MustRegister(memoryLimit)
-	prometheus.MustRegister(cpuUsagePercent)
-}
-
 func main() {
 
+	prometheus.MustRegister(collectors.NewDockerStatsCollector())
 	http.Handle("/metrics", promhttp.Handler())
-
-	go func() {
-		for {
-			cli, err := client.NewEnvClient()
-			if err != nil {
-				panic(err)
-			}
-
-			ctx := context.Background()
-
-			containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
-			if err != nil {
-				panic(err)
-			}
-
-			for _, container := range containers {
-				stats, err := cli.ContainerStats(ctx, container.ID, true)
-				if err != nil {
-					panic(err)
-				}
-				s := &types.Stats{}
-				json.NewDecoder(stats.Body).Decode(s)
-
-				memoryUsage.WithLabelValues(container.ID, container.Names[0], container.Labels["com.docker.compose.project"]).Set(float64(s.MemoryStats.Usage))
-				memoryLimit.WithLabelValues(container.ID, container.Names[0], container.Labels["com.docker.compose.project"]).Set(float64(s.MemoryStats.Limit))
-				cpuUsagePercent.WithLabelValues(container.ID, container.Names[0], container.Labels["com.docker.compose.project"]).Set(utils.CalculateCPUPercentUnix(s.PreCPUStats, s.CPUStats))
-			}
-		}
-	}()
 
 	err := http.ListenAndServe(":9101", nil)
 	if err != nil {
